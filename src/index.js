398c6404652bd9f54cd903873c09d16f7c531502
Richard Memory <richardfmemory@churchofjesuschrist.org>
2019-06-08 13:03:12 -0600

    Added a simple math.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import theCity, {
 	name,
 	getGreeting
 } from './myModule';
+import sumFunc, {
+	subtract
+} from './math.js'
 
 console.log(message);
 console.log(name);
 console.log(theCity);
 console.log(getGreeting("Richard"));
+
+console.log(`Adding 1 + 2 = ${sumFunc(1, 2)}`);
+console.log(`Subtracting 1 from 2 is ${subtract(2, 1)}`);
---
+++ src/math.js
@@ -0,0 +1,13 @@
+const add = (arg1, arg2) => {
+	return arg1 + arg2;
+}
+
+const subtract = (arg1, arg2) => {
+	return arg1 - arg2;
+}
+
+export {
+	add as
+	default,
+	subtract
+}
